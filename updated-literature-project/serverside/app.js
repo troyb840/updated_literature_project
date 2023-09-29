e596c7591599db18e3c96d5b0d91a050405dc84b
Daniel Pimentel <you@example.com>
2023-09-28 23:20:48 -0400

    initial commit

---
+++ updated-literature-project/serverside/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser  = require('body-parser');
+const request = require('request');
+const axios = require('axios');
+
+
+const mongoose = require('mongoose');
+//specify where to find the schema
+
+
+
+
+//specify which domains can make requests and which methods are allowed
+app.use((req, res, next) => {
+    console.log('This line is always called');
+    res.setHeader('Access-Control-Allow-Origin', '*'); //can connect from any host
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE'); //allowable methods
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//parse application/json
+app.use(bodyParser.json())
+
+///////////API Call 
+
+app.get('/api_test', (req, res, next) => {
+    //we will add an array named students to pretend that we received this data from the database
+    const students = [ 
+    { "id" : "1", "firstName" : "John" , "lastName" : "Dow" }, 
+    { "id" : "2", "firstName" : "Ann" , "lastName" : "Smith" }, 
+    { "id" : "3", "firstName" : "Joan" , "lastName" : "Doe" }];
+    //send the array as the response 
+    res.json(students);
+
+});
+
+
+//to use this middleware in other parts of the application
+module.exports=app;
