a71615b0bee86ba26e45f9ea14667d83e00416fb
Daniel Pimentel <you@example.com>
2023-09-28 23:46:32 -0400

    updates

--- updated-literature-project/serverside/models/citation.js
+++ updated-literature-project/serverside/models/citation.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 
-//define a schema/ blueprint NOTE: id is not a part of the schema 
-const postSchema = new mongoose.Schema({
-    firstName:  { type: String, required: true},
-    lastName:  { type: String, required: true},
-    organizationName:  { type: String, required: true},
-    organizationWebsite:  { type: String, required: true},
-    projectTitle:  { type: String, required: true},
+const citationSchema = new mongoose.Schema({
+    author:  { type: String, required: true},
+    year:  { type: String, required: true},
+    title:  { type: String, required: true},
+    section:  { type: String, required: true},
+    full_citation:  { type: String, required: true},
    
 });
 
-//use the blueprint to create the model 
 //Parameters: (model_name, schema_to_use, collection_name)
 //module.exports is used to allow external access to the model  
-module.exports = mongoose.model('Post', postSchema,'projects');
-//note capital S in the collection name
+module.exports = mongoose.model('Citation', citationSchema,'sources');
