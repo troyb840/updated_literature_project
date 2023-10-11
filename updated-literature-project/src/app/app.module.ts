dc9831da34c4277b9bd75351ffbcad4d520a3e8e
Troy Brown <troyb840@gmail.com>
2023-10-10 21:19:13 -0400

    Update app.module.ts

--- updated-literature-project/src/app/app.module.ts
+++ updated-literature-project/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule, ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
