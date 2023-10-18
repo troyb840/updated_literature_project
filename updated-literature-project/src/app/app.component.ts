69c214f8182dd59ad7c4580929c79f13f8932ce5
Troy Brown <troyb840@gmail.com>
2023-10-18 00:48:01 -0400

    Update app.component.ts

--- updated-literature-project/src/app/app.component.ts
+++ updated-literature-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'updated-literature-project';
+  searchForm: FormGroup;
+  selectedOutputFormat: string;
+
+  outputFormats = ['IEEE', 'ACM', 'APA'];
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      authorQuery: ['', [Validators.pattern('^[a-zA-Z ,.]*$')]],
+      yearQuery: ['', [Validators.pattern('^[0-9]*$')]],
+      titleQuery: ['', [Validators.pattern('^[a-zA-Z0-9 ]*$')]],
+      sectionQuery: ['', [Validators.pattern('^[a-zA-Z0-9 ]*$')]]
+    });
+  }
+
+  search() {
+    if (this.searchForm.valid) {
+      const formData = this.searchForm.value;
+      console.log('Search initiated', formData);
+    }
+  }
+
+  changeOutputFormat(event: any) {
+    this.selectedOutputFormat = event.target.value;
+  }
 }
