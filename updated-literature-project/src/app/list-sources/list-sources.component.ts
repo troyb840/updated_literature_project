dfbe1df8129997eae41cca504ad6cdc52cffb1f5
Troy Brown <troyb840@gmail.com>
2023-11-13 23:00:47 -0500

    Update list-sources.component.ts

--- updated-literature-project/src/app/list-sources/list-sources.component.ts
+++ updated-literature-project/src/app/list-sources/list-sources.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SourcesService } from '../sources.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-sources',
@@ -8,11 +9,26 @@ import { SourcesService } from '../sources.service';
 })
 export class ListSourcesComponent implements OnInit{
   public sources: any;
+  sortedSources: any[]; // This will hold the sorted sources
+  selectedSortOption: string;
 
-  constructor(private _myService: SourcesService) { }
-ngOnInit() {
+
+  constructor(private route: ActivatedRoute, private _myService: SourcesService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      // Fetch sources from your service (replace 'getSources()' with your actual method)
+      this.sources = this._myService.getSources();
+      // Initialize with default sort option
+      this.selectedSortOption = 'ascendingAuthor';
+      this.sortSources();
+    });
+
     this.getSources();
-}
+  }
+
+
+
 getSources(){
   this._myService.getSources().subscribe(
     data => {this.sources = data},
@@ -20,5 +36,47 @@ getSources(){
     () => console.log ('finished loading')
   
 );
+}
+sortSources(): void {
+  // Sort sources based on the selected option
+  this.sortedSources = this.sources.slice(); // Create a copy to avoid modifying the original array
+
+  switch (this.selectedSortOption) {
+    case 'ascendingAuthor':
+      this.sortedSources.sort((a, b) => a.authors.localeCompare(b.authors));
+      break;
+    case 'descendingAuthor':
+      this.sortedSources.sort((a, b) => b.authors.localeCompare(a.authors));
+      break;
+    case 'ascendingTitle':
+      this.sortedSources.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case 'descendingTitle':
+      this.sortedSources.sort((a, b) => b.title.localeCompare(a.title));
+      break;
+    case 'ascendingYear':
+      this.sortedSources.sort((a, b) => a.year.localeCompare(b.year));
+      break;
+    case 'descendingYear':
+      this.sortedSources.sort((a, b) => b.year.localeCompare(a.year));
+      break;
+    case 'ascendingSection':
+      this.sortedSources.sort((a, b) => a.section.localeCompare(b.section));
+      break;
+    case 'adecendingSection':
+       this.sortedSources.sort((a, b) => b.section.localeCompare(b.section));
+       break;
+    case 'ascendingCitation':
+       this.sortedSources.sort((a, b) => a.full_citation.localeCompare(b.full_citation));
+       break;
+    case 'descendingCitation':
+       this.sortedSources.sort((a, b) => b.full_citation.localeCompare(a.full_citation));
+       break;
+  
+              
+    // Add cases for other sorting options
+    default:
+      break;
+  }
 }
 }
